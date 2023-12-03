24ff1eb42f0e3d383b4c47d0ac05939a9429366e
petepen-art <petepen.art@gmail.com>
2023-12-03 18:30:51 -0300

    Config: Github

--- components/home/demo-modal.tsx
+++ components/home/demo-modal.tsx
@@ -30,7 +30,7 @@ const DemoModal = ({
           </a>
           <h3 className="font-display text-2xl font-bold">Precedent</h3>
           <p className="text-sm text-gray-500">
-            PETER E PENELOPE
+            Peter & Penelope, gozando juntos desde 2014
           </p>
         </div>
       </div>
